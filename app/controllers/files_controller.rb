52a839c32c517b61f730a638a2479d871a21c800
Rafael Alba <rafael@tcell.io>
2017-02-15 11:22:34 -0800

    see if this fixes waitlist issues

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,18 +1,49 @@
 class FilesController < ApplicationController
   skip_before_action :authenticate_user!
 
-  include ActionController::Live
+  #include ActionController::Live
+
+  # TODO: this doesn't work :(
+  #def stream
+    #response.headers["Content-Type"] = "text/event-stream"
+    #100.times {
+      #response.stream.write "<p>#{Time.now}</p>"
+      #sleep 1
+    #}
+  #ensure
+    #response.stream.close
+  #end
 
   def big_transfer
-    headers['X-Accel-Buffering'] = 'no' # Stop NGINX from buffering
-    headers["Cache-Control"] = "no-cache" # Stop downstream caching
-    headers["Transfer-Encoding"] = "chunked" # Chunked response header
-    headers.delete("Content-Length") # See one line above
-
     respond_to do |format|
-      format.html { render stream: true, layout: false }
-      format.csv { render stream: true, layout: false }
+      format.html {
+        #headers["X-Accel-Buffering"] = "no"
+        #headers["Cache-Control"] = "no-cache"
+        #headers["Content-Type"] = "text/html; charset=utf-8"
+        #headers["Transfer-Encoding"] = "chunked" # Chunked response header
+        #headers.delete("Content-Length") # See one line above
+        render stream: true
+      }
+      format.csv {
+        headers["X-Accel-Buffering"] = "no"
+        headers["Cache-Control"] = "no-cache"
+        headers["Content-Type"] = "text/csv; charset=utf-8"
+        #headers["Transfer-Encoding"] = "chunked" # Chunked response header
+        #headers["Content-Disposition"] =
+           #%(attachment; filename="report-#{Time.zone.now.to_date.to_s(:default)}.csv")
+        #headers["Last-Modified"] = Time.zone.now.ctime.to_s
+        #render stream: true, layout: false
+        #self.response_body = build_csv_enumerator(header, data)
+        self.response_body = Enumerator.new do |lines|
+          sleep 5
+          10.times do |x|
+            lines << (1..10).to_a.shuffle.join(",")
+          end
+        end
+      }
     end
+  ensure
+    response.stream.close
   end
 
   def big_transfer_file
